6754919f1849e07103a7b59505829098866d7a63
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2016-04-12 12:57:37 +0200

    Add cmake scripts and initial folder structure
    CMake creates a different library per folder in libraries. Building the
    entire library as one should be added later on.

---
+++ cmake/DefaultLibrary.cmake
@@ -0,0 +1,39 @@
+cmake_minimum_required(VERSION 2.8)
+cmake_policy(SET CMP0022 OLD)
+if(${CMAKE_MAJOR_VERSION} GREATER 2)
+    cmake_policy(SET CMP0038 OLD)
+endif()
+
+get_filename_component(ProjectId ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+string(REPLACE " " "_" ProjectId ${ProjectId})
+project(${ProjectId})
+
+include_directories(
+    ${OPENGL_INCLUDE_PATH}
+    ${GLEW_INCLUDE_PATH}
+    ${GLFW3_INCLUDE_PATH}
+    ${GLM_INCLUDE_PATH}
+    ${ASSIMP_INCLUDE_DIRS}
+    ${OpenCL_INCLUDE_DIRS}
+    ${EXTERNAL_LIBRARY_PATHS}
+    ${LIBRARIES_PATH}
+)
+
+file(GLOB_RECURSE SOURCES *.cpp)
+file(GLOB_RECURSE HEADER *.h)
+
+add_definitions(-DSHADERS_PATH="${SHADERS_PATH}")
+add_definitions(-DOPENCL_KERNEL_PATH="${OPENCL_KERNEL_PATH}")
+#add_definitions(-DRESOURCES_PATH="${RESOURCES_PATH}")
+add_definitions(-DGLFW_INCLUDE_GLCOREARB)
+add_definitions(-DGLEW_STATIC)
+
+target_link_libraries(
+    ${ProjectId}
+    ${ALL_LIBRARIES}
+    ${GLFW3_LIBRARIES}
+    ${GLEW_LIBRARIES}
+    ${OPENGL_LIBRARIES}
+    ${ASSIMP_LIBRARIES}
+    ${OPENCL_LIBRARIES}
+)
