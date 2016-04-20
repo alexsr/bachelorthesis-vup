77740916ca9f63b312159f1624e926aee217f1ba
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2016-04-20 11:32:59 +0200

    Create file for defines and common includes

---
+++ src/libraries/vup/defs.h
@@ -0,0 +1,13 @@
+#ifndef VUP_DEFS_H
+#define VUP_DEFS_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+#include <glm/gtc/matrix_access.hpp>
+
+#endif
