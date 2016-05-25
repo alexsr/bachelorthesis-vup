840e68115175efc491655e2b63e060691e65336f
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2016-05-25 22:01:37 +0200

    Add file for init functions

---
+++ src/libraries/vup/init.h
@@ -0,0 +1,35 @@
+#ifndef VUP_INIT_H
+#define VUP_INIT_H
+
+#include "vup/defs.h"
+#include <iostream>
+
+namespace vup {
+
+GLFWwindow* createWindow(int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share) {
+  GLint glfwError = glfwInit();
+  GLFWwindow* window = glfwCreateWindow(width, height, title, monitor, share);
+  if (window == nullptr) {
+    std::cout << "Failed to create window" << std::endl;
+    glfwTerminate();
+    exit(-1);
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwMakeContextCurrent(window);
+  return window;
+}
+
+void initGLEW() {
+  GLenum glewError = glewInit();
+  if (GLEW_OK != glewError) {
+    std::cout << "Failed to initialize GLEW: " << glewGetErrorString(glewError) << std::endl;
+    exit(-1);
+  }
+}
+
+}
+
+#endif
