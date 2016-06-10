df44d8faf8aa5310d4eab931f5b5c888aeb795fc
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2016-06-10 15:28:53 +0200

    Update example and send particle struct to kernel

--- examples/cl_instanced_renderer_example/cl_instanced_renderer_example.cpp
+++ examples/cl_instanced_renderer_example/cl_instanced_renderer_example.cpp
@@ -59,7 +59,21 @@ int main()
   vup::type type(particle_amount);
   for (int i = 0; i < particle_amount; i++) {
     type[i] = static_cast<int>(-1.0f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / 2.0f)));
-
+  }
+  std::vector<vup::particle> particles(particle_amount);
+  for (int i = 0; i < particle_amount; i++) {
+    particles[i].id = 0;
+    particles[i].type = 0;
+    particles[i].mass = 5.0f;
+    particles[i].density = 3.0f;
+    particles[i].viscosity = 3.0f;
+  }
+  for (int i = 0; i < particle_amount/2; i++) {
+    particles[i].id = 0;
+    particles[i].type = 0;
+    particles[i].mass = 1.0f;
+    particles[i].density = 3.0f;
+    particles[i].viscosity = 3.0f;
   }
 
   vup::OpenCLBasis clBasis(1, CL_DEVICE_TYPE_GPU, 0);
@@ -69,7 +83,7 @@ int main()
 
   float size = .1f;
   vup::SphereData sphere(size, 20, 20);
-  vup::ParticleRenderer renderer(sphere, particle_amount, buffers.getInteropVBOs());
+  vup::ParticleRenderer renderer(sphere, buffers.getInteropVBOs());
 
   std::vector<int> fluidIndices;
   for (int i = 0; i < 1000; i++) {
@@ -82,9 +96,11 @@ int main()
 
   // OPENCL
   vup::ParticleQueue queue(clBasis.context(), particle_amount);
-  buffers.addGL("pos_vbo", CL_MEM_READ_WRITE, "pos");
-  buffers.add("vel", CL_MEM_READ_WRITE, sizeof(glm::vec4) * vel.size());
-  queue.writeBuffer(buffers.get("vel"), sizeof(glm::vec4) * vel.size(), &vel[0]);
+  buffers.createBufferGL("pos_vbo", CL_MEM_READ_WRITE, "pos");
+  buffers.createBuffer<vup::velocity>("vel", CL_MEM_READ_WRITE, vel.size());
+  queue.writeBuffer(buffers.getBuffer("vel"), sizeof(glm::vec4) * vel.size(), &vel[0]);
+  buffers.createBuffer<vup::particle>("particles", CL_MEM_READ_WRITE, particles.size());
+  queue.writeBuffer(buffers.getBuffer("particles"), sizeof(vup::particle) * particles.size(), &particles[0]);
  // queue.setTypeIndices(VUP_FLUID, CL_MEM_READ_WRITE, fluidIndices, particle_amount);
   //queue.setTypeIndices(VUP_RIGID, CL_MEM_READ_WRITE, rigidIndices, particle_amount);
   
@@ -95,17 +111,18 @@ int main()
   vup::KernelHandler kh(clBasis.context(), clBasis.device(), OPENCL_KERNEL_PATH "/fakebox.cl", { "test", "fakecollision" });
   
   kh.initKernel("move");
-  kh.setArg("move", 0, buffers.getGL("pos_vbo"));
-  kh.setArg("move", 1, buffers.get("vel"));
+  kh.setArg("move", 0, buffers.getBufferGL("pos_vbo"));
+  kh.setArg("move", 1, buffers.getBuffer("vel"));
   kh.setArg("move", 2, dt);
  // kh.setArg("move", 3, queue.getIndexBuffer(VUP_FLUID));
   //kh.initKernel("test");
-  kh.setArg("test", 0, buffers.getGL("pos_vbo"));
-  kh.setArg("test", 1, buffers.get("vel"));
-  kh.setArg("test", 2, dt);
+  kh.setArg("test", 0, buffers.getBufferGL("pos_vbo"));
+  kh.setArg("test", 1, buffers.getBuffer("vel"));
+  kh.setArg("test", 2, buffers.getBuffer("particles"));
+  kh.setArg("test", 3, dt);
  // kh.initKernel("fakecollision");
-  kh.setArg("fakecollision", 0, buffers.getGL("pos_vbo"));
-  kh.setArg("fakecollision", 1, buffers.get("vel"));
+  kh.setArg("fakecollision", 0, buffers.getBufferGL("pos_vbo"));
+  kh.setArg("fakecollision", 1, buffers.getBuffer("vel"));
   kh.setArg("fakecollision", 2, dt);
   kh.setArg("fakecollision", 3, particle_amount);
   
@@ -126,12 +143,13 @@ int main()
     int state = glfwGetKey(window, GLFW_KEY_R);
     if (state == GLFW_PRESS) {
       kh.reloadProgram();
-      kh.setArg("test", 0, buffers.getGL("pos_vbo"));
-      kh.setArg("test", 1, buffers.get("vel"));
-      kh.setArg("test", 2, dt);
+      kh.setArg("test", 0, buffers.getBufferGL("pos_vbo"));
+      kh.setArg("test", 1, buffers.getBuffer("vel"));
+      kh.setArg("test", 2, buffers.getBuffer("particles"));
+      kh.setArg("test", 3, dt);
       // kh.initKernel("fakecollision");
-      kh.setArg("fakecollision", 0, buffers.getGL("pos_vbo"));
-      kh.setArg("fakecollision", 1, buffers.get("vel"));
+      kh.setArg("fakecollision", 0, buffers.getBufferGL("pos_vbo"));
+      kh.setArg("fakecollision", 1, buffers.getBuffer("vel"));
       kh.setArg("fakecollision", 2, dt);
       kh.setArg("fakecollision", 3, particle_amount);
     }
