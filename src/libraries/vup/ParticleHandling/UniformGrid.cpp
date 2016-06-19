5a161fad7dd225c8d7b6426faf2e0492529f816d
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2016-06-19 20:41:26 +0200

    Update var names in uniform grid

--- src/libraries/vup/ParticleHandling/UniformGrid.cpp
+++ src/libraries/vup/ParticleHandling/UniformGrid.cpp
@@ -1,27 +1,27 @@
 #include "UniformGrid.h"
 
-vup::UniformGrid::UniformGrid(int maxNeighbors, float cellsize, float gridlength, cl::Context context, cl_mem_flags flags)
+vup::UniformGrid::UniformGrid(int cellCapacity, float cellsize, float gridradius, cl::Context context, cl_mem_flags flags)
 {
-  m_maxNeighbors = maxNeighbors;
-  m_gridlength = gridlength;
+  m_cellCapacity = cellCapacity;
+  m_gridradius = gridradius;
   m_cellsize = cellsize;
-  m_lineSize = m_gridlength / m_cellsize;
+  m_lineSize = (int) 2.0f * m_gridradius / m_cellsize;
   m_cellamount = m_lineSize * m_lineSize * m_lineSize;
-  m_maxGridCapacity = (int) m_maxNeighbors * m_cellamount;
+  m_maxGridCapacity = (int) m_cellCapacity * m_cellamount;
   m_cellContents = std::vector<int>(m_maxGridCapacity);
   m_neighborCounter = std::vector<int>(m_cellamount);
   for (int i = 0; i < m_lineSize; i++) {
     for (int j = 0; j < m_lineSize; j++) {
       for (int k = 0; k < m_lineSize; k++) {
         m_neighborCounter[i * m_lineSize * m_lineSize + j * m_lineSize + k] = 0;
-        for (int n = 0; n < m_maxNeighbors; n++) {
-          m_cellContents[i * m_lineSize * m_lineSize * m_maxNeighbors + j * m_lineSize * m_maxNeighbors + k * m_maxNeighbors + n] = -1;
+        for (int n = 0; n < m_cellCapacity; n++) {
+          m_cellContents[i * m_lineSize * m_lineSize * m_cellCapacity + j * m_lineSize * m_cellCapacity + k * m_cellCapacity + n] = -1;
         }
       }
     }
   }
   cl_int clError;
-  m_cellBuffer = cl::Buffer(context, flags, m_maxGridCapacity * sizeof(int), &m_cellContents[0], &clError);
+  m_gridBuffer = cl::Buffer(context, flags, m_maxGridCapacity * sizeof(int), &m_cellContents[0], &clError);
   if (clError != CL_SUCCESS) {
     throw vup::BufferCreationException("Uniform grid cell buffer", clError);
   }
--- src/libraries/vup/ParticleHandling/UniformGrid.h
+++ src/libraries/vup/ParticleHandling/UniformGrid.h
@@ -13,22 +13,22 @@ namespace vup {
 
 class UniformGrid {
 public:
-  UniformGrid(int maxNeighbors, float cellsize, float gridlength, cl::Context context, cl_mem_flags flags);
+  UniformGrid(int maxNeighbors, float cellsize, float gridradius, cl::Context context, cl_mem_flags flags);
   ~UniformGrid();
-  cl::Buffer getCellBuffer() { return m_cellBuffer; }
+  cl::Buffer getGridBuffer() { return m_gridBuffer; }
   cl::Buffer getNeighborCounterBuffer() { return m_neighborCounterBuffer; }
   int getCellAmount() { return m_cellamount; }
   int getMaxGridCapacity() { return m_maxGridCapacity; }
-  int getMaxNeighbors() { return m_maxNeighbors; }
+  int getMaxNeighbors() { return m_cellCapacity; }
   int getLineSize() { return m_lineSize; }
 private:
-  int m_maxNeighbors;
+  int m_cellCapacity;
   int m_cellamount;
   int m_maxGridCapacity;
   int m_lineSize;
-  float m_gridlength;
+  float m_gridradius;
   float m_cellsize;
-  cl::Buffer m_cellBuffer;
+  cl::Buffer m_gridBuffer;
   cl::Buffer m_neighborCounterBuffer;
   std::vector<int> m_cellContents;
   std::vector<int> m_neighborCounter;
